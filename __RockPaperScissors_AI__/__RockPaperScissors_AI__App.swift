045877e8283a6995643af0962e41007a54a72f2e
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2025-03-01 14:17:41 +0600

    Initial Commit

---
+++ __RockPaperScissors_AI__/__RockPaperScissors_AI__App.swift
@@ -0,0 +1,17 @@
+//
+//  __RockPaperScissors_AI__App.swift
+//  __RockPaperScissors_AI__
+//
+//  Created by  Sadi on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct __RockPaperScissors_AI__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
